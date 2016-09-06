1ccc6431ff00eac71b1df645427a5a5a5c63a1f6
Christian <cschomak@cern.ch>
2016-09-06 17:04:10 +0200

    Double sided CB included in RooFit in ROOT 6 per default, removed from compile shapes

--- compileShapes.sh
+++ compileShapes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-root -l shapes/RooSUSYTPdf.cxx++ shapes/RooSUSYX4Pdf.cxx++ shapes/RooSUSYXM4Pdf.cxx++ shapes/RooSUSYBkgMAPdf.cxx++ shapes/RooSUSYBkgMoreParamsPdf.cxx++ shapes/RooSUSYBkgPdf.cxx++ shapes/RooSUSYOldBkgPdf.cxx++ shapes/RooSUSYBkgGaussiansPdf.cxx++ shapes/RooDoubleCB.cxx++ shapes/RooTopPairProductionSpline.cxx++
+root -l shapes/RooSUSYTPdf.cxx++ shapes/RooSUSYX4Pdf.cxx++ shapes/RooSUSYXM4Pdf.cxx++ shapes/RooSUSYBkgMAPdf.cxx++ shapes/RooSUSYBkgMoreParamsPdf.cxx++ shapes/RooSUSYBkgPdf.cxx++ shapes/RooSUSYOldBkgPdf.cxx++ shapes/RooSUSYBkgGaussiansPdf.cxx++ shapes/RooTopPairProductionSpline.cxx++
